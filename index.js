a1e24cf5504e8ec4838c138aafafc6c468650a02
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2018-06-10 19:56:04 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,55 @@
+const Discord = require('discord.js');
+var bot = new Discord.Client();
+const prefix = "!";
+var request = require('request');
+
+
+bot.on("guildMemberAdd", function(member) {
+    
+    var role_rules = member.guild.roles.find('name', 'Unaccepted Rules');
+    member.addRole(role_rules)
+});
+
+
+
+bot.on("ready", function() {
+    bot.user.setActivity('m1nty.eu');
+    console.log("Im Ready!");
+    global = bot.channels.find("name", "welcome").fetchMessage("455423491152412694");
+});
+    
+bot.on('messageReactionAdd', function(reaction, users) {
+    console.log("kebab")
+    var Welcome = bot.channels.find("name", "welcome");
+    var messageid = global;
+    var Emoji = "✅";
+    var rolenews = reaction.message.guild.roles.find("name", "agreed");
+    if (!reaction.message == global) return;
+    if (!reaction.emoji.name == Emoji) return;
+    reaction.message.guild.members.get(users.id).addRole(rolenews);
+});
+
+bot.on('messageReactionRemove', function(reaction, users) {
+    console.log("no kebab")
+    var Welcome = bot.channels.find("name", "welcome");
+    var messageid = global;
+    var Emoji = "✅";
+    var rolenews = reaction.message.guild.roles.find("name", "agreed");
+    if (!reaction.message == global) return;
+    if (!reaction.emoji.name == Emoji) return;
+    reaction.message.guild.members.get(users.id).removeRole(rolenews);
+});
+
+bot.on("message", function(message) {
+
+    var msg = message.content.toLowerCase();
+    var cont = message.content.slice(prefix.length).split(" ");
+    var args = cont.slice(1);
+    var msgauthor = message.author;
+    var channel1 = bot.channels.find('name', 'readme');
+    var rcchanel = bot.channels.find("name", "recruitments");
+
+    
+});
+
+bot.login("NDU1MDkxMjc5Mjk5MzQ2NDMz.Df28TQ.t_AYgp2L8KLK5BK5zO3r5MEH7DU");
