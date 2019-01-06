16e05f7c8caffc97ac1dcf02c7b79c87a0da6b12
Osmulak122 <34412964+Osmulak122@users.noreply.github.com>
2019-01-06 13:02:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ bot.on("message", function(message) {
             return message.channel.send("Available socials :\ntriguizz, minty");
     }       
     if (scmb == "minty") {
-        message.channel.send(`**Socials of ${scmb} :**`+"\nhttp://m1nty.eu/\nhttp://twitter.m1nty.eu/\nhttp://youtube.m1nty.eu/");
+        message.channel.send(`**Socials of ${scmb} :**`+"\n<http://m1nty.eu/>\n<http://twitter.m1nty.eu/>\n<http://youtube.m1nty.eu/>");
     }
     if (scmb == "triguizz") {
         message.channel.send(`**Socials of ${scmb} :**`+"\n<https://twitter.com/TriGuizz>\n<https://www.youtube.com/channel/UC5Q1S26eV3ivO2gmLldCN6w>\n<https://steamcommunity.com/id/TriGuizz/>");
